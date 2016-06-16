007ebfdabecab418defc7b9fd9120fb364f04bf3
VictorJGaxiola <vic.gaxiola@gmail.com>
2016-06-15 20:42:23 -0500

    Alumnos ahora se muestran en paginas

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -2,6 +2,11 @@ class PagsController < ApplicationController
 	def pag3
 		
 	end
+
+	before_action :get_data
+	
+	private
+
 	def get_data
 		@users = ["Alumno1","Alumno2","Alumno3","Alumno4"]
 	end
